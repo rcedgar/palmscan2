4b038d88568c0a0ece98fd37bf88118ba266256f
Robert Edgar <robert@drive5.com>
2024-03-24 15:20:25 -0700

    FPR 0.0654 at TPR=0.9 on dss_calibrated (0.09 uncalibrated, foldseek 0.03, dali 0.008)

--- scop40bit_calibrate_dss.cpp
+++ scop40bit_calibrate_dss.cpp
@@ -5,7 +5,6 @@
 
 void cmd_scop40bit_calibrate_dss()
 	{
-	asserta(optset_tsv);
 	const string &FN = g_Arg1;
 	SCOP40Bit SB;
 	SB.ReadDomInfo();
@@ -37,33 +36,66 @@ void cmd_scop40bit_calibrate_dss()
 			}
 		}
 
-	float Sumx = 0;
-	float Sumx2 = 0;
-	float Sumy = 0;
-	float Sumy2 = 0;
-	float Sumxy = 0;
-	for (uint DomIdx = 0; DomIdx < DomCount; ++DomIdx)
+	vector<float> Pows;
+	//Pows.push_back(0.1f);
+	//Pows.push_back(0.2f);
+	//Pows.push_back(0.4f);
+	Pows.push_back(0.5f);
+	//Pows.push_back(0.6f);
+	//Pows.push_back(0.7f);
+	//Pows.push_back(0.8f);
+	//Pows.push_back(1.0f);
+	//Pows.push_back(1.5f);
+	//Pows.push_back(2.0f);
+	//Pows.push_back(3.0f);
+	vector<float> Vars;
+	for (vector<float>::const_iterator iter = Pows.begin();
+	  iter != Pows.end(); ++iter)
 		{
-		const string &DomName = SB.m_Doms[DomIdx];
-		uint n = NegativeScoreCounts[DomIdx];
-		float Sum2 = SumScore2[DomIdx];
-		float ScoreFirstFP = DomToScoreFirstFP[DomIdx];
-		ScoreFirstFP = powf(ScoreFirstFP, 0.2);
-		float Var = 0;
-		if (n > 0)
-			Var = sqrtf(Sum2/n);
-		Sumx += Var;
-		Sumx2 += Var*Var;
-		Sumy += ScoreFirstFP;
-		Sumy2 += ScoreFirstFP*ScoreFirstFP;
-		Sumxy += Var*ScoreFirstFP;
-		fprintf(g_ftsv, "%u\t%s\t%u\t%.3g\t%.3g\n",
-		  DomIdx, DomName.c_str(), n, Var, ScoreFirstFP);
+		float Pow = *iter;
+
+		float Sumx = 0;
+		float Sumx2 = 0;
+		float Sumy = 0;
+		float Sumy2 = 0;
+		float Sumxy = 0;
+		for (uint DomIdx = 0; DomIdx < DomCount; ++DomIdx)
+			{
+			const string &DomName = SB.m_Doms[DomIdx];
+			uint n = NegativeScoreCounts[DomIdx];
+			float Sum2 = SumScore2[DomIdx];
+			float ScoreFirstFP = DomToScoreFirstFP[DomIdx];
+			float Var = 0;
+			if (n > 0)
+				Var = sqrtf(Sum2/n);
+			Var = powf(Var, Pow);
+			Vars.push_back(Var);
+			Sumx += Var;
+			Sumx2 += Var*Var;
+			Sumy += ScoreFirstFP;
+			Sumy2 += ScoreFirstFP*ScoreFirstFP;
+			Sumxy += Var*ScoreFirstFP;
+			Pf(g_ftsv, "%u\t%s\t%u\t%.3g\t%.3g\n",
+			  DomIdx, DomName.c_str(), n, Var, ScoreFirstFP);
+			}
+
+		float top = Sumxy - (Sumx*Sumy)/DomCount;
+		float bottomL = Sumx2 - (Sumx*Sumx)/DomCount;
+		float bottomR = Sumy2 - (Sumy*Sumy)/DomCount;
+		float Correl = top/sqrtf(bottomL*bottomR);
+		ProgressLog("Pow = %.2f correl = %.4f\n", Pow, Correl);
 		}
 
-	float top = Sumxy - (Sumx*Sumy)/DomCount;
-	float bottomL = Sumx2 - (Sumx*Sumx)/DomCount;
-	float bottomR = Sumy2 - (Sumy*Sumy)/DomCount;
-	float Correl = top/sqrtf(bottomL*bottomR);
-	ProgressLog("Correl = %.3g\n", Correl);
+	if (optset_output)
+		{
+		for (uint i = 0; i < HitCount; ++i)
+			{
+			uint DomIdx1 = SB.m_DomIdx1s[i];
+			float Score = SB.m_Scores[i];
+			float Var = Vars[DomIdx1];
+			SB.m_Scores[i] = Score/Var;
+			}
+
+		SB.WriteHits_Bin(opt_output);
+		}
 	}
